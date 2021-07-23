db93e6ee40ff0c2dbbba9a3ab1226bacb8b0e4a2
Plusb Preco <plusb21@gmail.com>
2021-07-24 02:02:43 +0900

    fix: set `ignoreStrings` option in `max-len` rule

--- rules/base/stylistic-issues.js
+++ rules/base/stylistic-issues.js
@@ -82,7 +82,7 @@ module.exports = {
       tabWidth: 2,
       ignoreComments: true,
       ignoreUrls: true,
-      ignoreStrings: false,
+      ignoreStrings: true,
       ignoreTemplateLiterals: true,
       ignoreRegExpLiterals: true,
     }],
