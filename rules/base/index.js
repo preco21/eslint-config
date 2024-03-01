133101b6371e9efcfa23f279654c5dd4906740e3
Preco Plusb <plusb21@gmail.com>
2024-03-02 02:04:16 +0900

    feat: use common env

--- rules/base/index.js
+++ rules/base/index.js
@@ -2,10 +2,16 @@
 
 module.exports = {
   env: {
-    es6: true,
+    // Allow all ECMAScript 2023 globals including ES2015(ES6)+.
+    // https://github.com/eslint/eslintrc/blob/289653be93bb3c0768d9a2d1b3f039b900c44e45/conf/environments.js#L117-L122
+    'es2023': true,
+    // Allow common web globals available in modern environments such as Node.js v18+, Deno and Bun.
+    // https://github.com/sindresorhus/globals/blob/64d801f42fd9a35617622b1b19010701352efe6d/globals.json#L1720-L1749
+    'shared-node-browser': true,
   },
   parserOptions: {
-    ecmaVersion: 2019,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   extends: [
     './possible-errors',
