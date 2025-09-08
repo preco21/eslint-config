23465024923d90a84b5ef76622b854f171c2cd9a
Preco Plusb <plusb21@gmail.com>
2025-09-08 12:33:03 +0900

    refactor: wip

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { antfu } from './factory'
+
+export * from './configs'
+export * from './factory'
+export * from './globs'
+export * from './types'
+export * from './utils'
+
+export default antfu
---
+++ src/utils.ts
@@ -0,0 +1,133 @@
+import type { Awaitable, TypedFlatConfigItem } from './types'
+
+import process from 'node:process'
+import { fileURLToPath } from 'node:url'
+
+const scopeUrl = fileURLToPath(new URL('.', import.meta.url))
+
+export const parserPlain = {
+  meta: {
+    name: 'parser-plain',
+  },
+  parseForESLint: (code: string) => ({
+    ast: {
+      body: [],
+      comments: [],
+      loc: { end: code.length, start: 0 },
+      range: [0, code.length],
+      tokens: [],
+      type: 'Program',
+    },
+    scopeManager: null,
+    services: { isPlain: true },
+    visitorKeys: {
+      Program: [],
+    },
+  }),
+}
+
+/**
+ * Combine array and non-array configs into a single array.
+ */
+export async function combine(...configs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[]>[]): Promise<TypedFlatConfigItem[]> {
+  const resolved = await Promise.all(configs)
+  return resolved.flat()
+}
+
+/**
+ * Rename plugin prefixes in a rule object.
+ * Accepts a map of prefixes to rename.
+ *
+ * @example
+ * ```ts
+ * import { renameRules } from '@antfu/eslint-config'
+ *
+ * export default [{
+ *   rules: renameRules(
+ *     {
+ *       '@typescript-eslint/indent': 'error'
+ *     },
+ *     { '@typescript-eslint': 'ts' }
+ *   )
+ * }]
+ * ```
+ */
+export function renameRules(
+  rules: Record<string, any>,
+  map: Record<string, string>,
+): Record<string, any> {
+  return Object.fromEntries(
+    Object.entries(rules)
+      .map(([key, value]) => {
+        for (const [from, to] of Object.entries(map)) {
+          if (key.startsWith(`${from}/`))
+            return [to + key.slice(from.length), value]
+        }
+        return [key, value]
+      }),
+  )
+}
+
+/**
+ * Rename plugin names a flat configs array
+ *
+ * @example
+ * ```ts
+ * import { renamePluginInConfigs } from '@antfu/eslint-config'
+ * import someConfigs from './some-configs'
+ *
+ * export default renamePluginInConfigs(someConfigs, {
+ *   '@typescript-eslint': 'ts',
+ *   '@stylistic': 'style',
+ * })
+ * ```
+ */
+export function renamePluginInConfigs(configs: TypedFlatConfigItem[], map: Record<string, string>): TypedFlatConfigItem[] {
+  return configs.map((i) => {
+    const clone = { ...i }
+    if (clone.rules)
+      clone.rules = renameRules(clone.rules, map)
+    if (clone.plugins) {
+      clone.plugins = Object.fromEntries(
+        Object.entries(clone.plugins)
+          .map(([key, value]) => {
+            if (key in map)
+              return [map[key], value]
+            return [key, value]
+          }),
+      )
+    }
+    return clone
+  })
+}
+
+export function toArray<T>(value: T | T[]): T[] {
+  return Array.isArray(value) ? value : [value]
+}
+
+export async function interopDefault<T>(m: Awaitable<T>): Promise<T extends { default: infer U } ? U : T> {
+  const resolved = await m
+  return (resolved as any).default || resolved
+}
+
+export function isInEditorEnv(): boolean {
+  if (process.env.CI)
+    return false
+  if (isInGitHooksOrLintStaged())
+    return false
+  return !!(false
+    || process.env.VSCODE_PID
+    || process.env.VSCODE_CWD
+    || process.env.JETBRAINS_IDE
+    || process.env.VIM
+    || process.env.NVIM
+  )
+}
+
+export function isInGitHooksOrLintStaged(): boolean {
+  return !!(false
+    || process.env.GIT_PARAMS
+    || process.env.VSCODE_GIT_COMMAND
+    || process.env.npm_lifecycle_script?.startsWith('lint-staged')
+  )
+}
